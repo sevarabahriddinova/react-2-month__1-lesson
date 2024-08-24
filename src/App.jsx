46060019964e10d87bf5f9c5f727b7d950591958
sevarabahriddinova <sevaraxonbahriddinova@gmail.com>
2024-08-24 22:59:30 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const [ country, setCountry]=useState
+
+  return (
+    <>
+       
+    </>
+  )
+}
+
+export default App
